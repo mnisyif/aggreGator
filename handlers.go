83562b3d1e210f889a91f9607bdd90c9644f2251
Murtadha <mnisyif@gmail.com>
2026-07-06 19:59:42 +0300

    feature: implement handlerRegistration to register new users

--- handlers.go
+++ handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/mnisyif/aggreGator/internal/commands"
+	"github.com/mnisyif/aggreGator/internal/database"
 )
 
 func handlerLogin(s *commands.State, cmd commands.Command) error {
@@ -18,5 +22,28 @@ func handlerLogin(s *commands.State, cmd commands.Command) error {
 
 	fmt.Println("Username has been set successfully")
 
+	return nil
+}
+
+func handlerRegister(s *commands.State, cmd commands.Command) error {
+	if len(cmd.Args) == 0 {
+		return fmt.Errorf("register command expects a username argument\n")
+	}
+
+	newUser := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.Args[0],
+	}
+
+	user, err := s.DB.CreateUser(context.Background(), newUser)
+	if err != nil {
+		return fmt.Errorf("could not register <%s> : %s\n", cmd.Args[0], err)
+	}
+
+	s.Cfg.SetUser(user.Name)
+	fmt.Println("User was created successfully")
+	fmt.Printf("ID: %s, Name: %s, Created At: %s\n", user.ID, user.Name, user.CreatedAt)
 	return nil
 }
