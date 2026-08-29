7468944c814608f0b1f7b45d1e74748ad7c51be3
Murtadha <mnisyif@gmail.com>
2026-08-29 14:28:04 +0300

    feature: new query to mark when feed was last fetched

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -19,5 +19,11 @@ SELECT * FROM feeds;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds WHERE url = $1;
 
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET updated_at = NOW(), last_fetched_at = NOW()
+WHERE id = $1
+RETURNING *;
+
 -- name: ResetFeeds :exec
 DELETE FROM feeds;
